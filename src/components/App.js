5363620adedd84bea318293d1ed17abddef1cd47
Renny Muragu <118157651+rennynjuguna@users.noreply.github.com>
2023-01-31 16:19:18 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [imageURL, setImageURL] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => {
+        setImageURL(data.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {!imageURL ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={imageURL} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
+
